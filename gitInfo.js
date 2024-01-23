7db4db5a6c4366d935daf7f504fbbf19bee53e54
Alejo32267 <123325333+Alejo32267@users.noreply.github.com>
2024-01-23 09:08:55 -0500

    Add files via upload

---
+++ gitInfo.js
@@ -0,0 +1,107 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+// Definition of Git
+let gitDefinition = "Git is a distributed version control system that allows multiple developers to collaborate on a project, tracking changes to the codebase and managing different versions of the code.";
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+// Definition of GitHub
+let gitHubDefinition = "GitHub is a web-based platform that provides hosting for software development version control using Git. It also offers additional features such as bug tracking, task management, and wikis for every project.";
+
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+// Definition of 'git init'
+let gitInitDefinition = "'git init' is a command used to initialize a new, empty Git repository in the current working directory.";
+
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+// Definition of 'git clone'
+let gitCloneDefinition = "'git clone' is a command used to create a copy of an existing Git repository, including all branches and commit history, to a new directory.";
+
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+// Definition of 'git status'
+let gitStatusDefinition = "'git status' is a command used to display the state of the working directory and the staging area. It shows which files have been modified, which files are staged, and which files are not being tracked by Git.";
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+// Definition of 'git add'
+let gitAddDefinition = "'git add' is a command used to stage changes, adding them to the staging area in preparation for a commit.";
+let gitAddCode = "git add ."; // Code to add all files
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+// Definition of 'git commit'
+let gitCommitDefinition = "'git commit' is a command used to record the changes made to the files in the repository. It requires a commit message that describes the changes.";
+let gitCommitCode = "git commit -m 'initial commit'"; // Code to commit using the message "initial commit"
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+// Definition of 'git push'
+let gitPushDefinition = "'git push' is a command used to upload local repository content to a remote repository. It transfers commits from the local branch to the remote branch on a specified remote location, such as GitHub.";
+
+
+
